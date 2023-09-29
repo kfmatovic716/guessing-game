7af81d4a99a0adb8dd54e5862e3fb47f3bcb8485
Aaron Torres <aarontorres@Aarons-MBP.lan>
2023-09-29 15:39:53 -0700

    added time function

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,39 @@
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 20;
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timeEl.textContent = "There are " + secondsLeft + " seconds left!";
+    
+    if(secondsLeft === 0){
+      clearInterval(timerInterval);
+
+      sendMessage()
+    }
+
+  }, 1000)
+}
+
+function sendMessage() {
+  timeEl.textContent = "Out of time! Play again."
+}
+
+
+
+
+
+
+
+
+
+/*function playGame()
+
+
+
+
+
 var startButton = document.querySelector("#theme-switcher");
 
 
@@ -8,23 +44,23 @@ startButton.addEventListener("click", function() {
 
     
 }
+var secondsLeft = 20;
 
 function setTime() {
-    // Sets interval in variable
-    //The setInterval() method takes a function as its first argument
     var timerInterval = setInterval(function() {
-      //The function is the action that we want to be evaluated at each interval. This function prints a message at each interval
+      
       secondsLeft--;
-      // secondsLeft = secondsLeft - 1;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+     
+      timeEl.textContent = ("You have " + secondsLeft + "left!");
   
       if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        //When the countdown reaches zero, we want the action to stop. The clearInterval() method stops the execution of the interval. It takes one parameter: the variable name assigned to the interval
+        
         clearInterval(timerInterval);
-        // Calls function to create and append image
+        
         sendMessage();
       }
-      //The second argument that the setInterval() method takes is the interval. Because intervals use milliseconds, we use 1000 to create an interval of one second
+      
     }, 1000);
   }
+
+  setTime() */
